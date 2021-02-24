cbd2781a21855a8e13fd8ae6644a52f84ab53d04
Nerea García <nereitagp12@gmail.com>
2021-02-24 18:01:35 +0100

    Added security to account

---
+++ src/main/java/com/ironhack/account/controller/impl/AuthController.java
@@ -0,0 +1,61 @@
+package com.ironhack.account.controller.impl;
+
+import com.ironhack.account.auth.dto.AuthenticationRequest;
+import com.ironhack.account.auth.dto.AuthenticationResponse;
+import com.ironhack.account.auth.service.MyUserDetailsService;
+import com.ironhack.account.auth.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    private static String contactAuthOk;
+
+    /** AUTHENTICATION **/
+    @PostMapping("account/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }
+        catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+    public static String getContactAuthOk() {
+        return contactAuthOk;
+    }
+
+    public static void setContactAuthOk(String contactAuthOk) {
+        AuthController.contactAuthOk = contactAuthOk;
+    }
+
+}
