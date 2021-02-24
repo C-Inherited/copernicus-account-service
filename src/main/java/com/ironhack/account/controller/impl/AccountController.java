57a9a678ab069dda8320aa59ae27edc3c70bf3df
Nerea García <nereitagp12@gmail.com>
2021-02-24 19:58:06 +0100

    Modified account controller for security

--- src/main/java/com/ironhack/account/controller/impl/AccountController.java
+++ src/main/java/com/ironhack/account/controller/impl/AccountController.java
@@ -11,36 +11,38 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-public class AccountController implements IAccountController {
+public class AccountController  {
 
     @Autowired
     private AccountService accountService;
 
     @GetMapping("/account/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public AccountDTO getAccount(@PathVariable (name = "id") Integer id){
+    public AccountDTO getAccount(@PathVariable(name = "id") Integer id, @RequestHeader(value = "Authorization") String authorizationHeader) {
         return accountService.getAccount(id);
     }
 
     @GetMapping("/accounts")
     @ResponseStatus(HttpStatus.OK)
-    public List<AccountDTO> getAllAccounts(){
+    public List<AccountDTO> getAllAccounts(@RequestHeader(value = "Authorization") String authorizationHeader) {
         return accountService.findAll();
     }
 
     @PostMapping("/account")
     @ResponseStatus(HttpStatus.CREATED)
-    public AccountDTO saveAccount(@RequestBody @Valid AccountDTO accountDTO){
+    public AccountDTO saveAccount(@RequestBody @Valid AccountDTO accountDTO, @RequestHeader(value = "Authorization") String authorizationHeader) {
         return accountService.createAccount(accountDTO);
     }
+
     @PutMapping("/account/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public AccountDTO updateAccount(@RequestBody @Valid AccountDTO accountDTO, @PathVariable (name = "id") Integer id){
+    public AccountDTO updateAccount(@RequestBody @Valid AccountDTO accountDTO, @PathVariable(name = "id") Integer id, @RequestHeader(value = "Authorization") String authorizationHeader) {
         return accountService.updateAccount(accountDTO, id);
     }
+
     @DeleteMapping("/account/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteAccount(@PathVariable (name = "id") Integer id){
+    public void deleteAccount(@PathVariable(name = "id") Integer id, @RequestHeader(value = "Authorization") String authorizationHeader) {
         accountService.deleteAccount(id);
     }
 }
